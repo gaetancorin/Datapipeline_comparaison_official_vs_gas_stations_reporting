1f473f099e47a9e2b5df7d9240ff1f1062721936
gaetancorin <gaetancorin@gmail.com>
2025-06-26 01:37:24 +0200

    add metabase db managements logicals in flask metabase

--- Projet_master_RNCP/project_master_METABASE/App/utils.py
+++ Projet_master_RNCP/project_master_METABASE/App/utils.py
@@ -1,22 +1,44 @@
 import shutil
 from pathlib import Path
 from datetime import datetime
+import subprocess
+import os
 
-def extract_metabase_db():
-    source_path = Path("/metabase_data/metabase.db.mv.db")
-    output_dir = Path("/outputs")
-    output_dir.mkdir(parents=True, exist_ok=True)
-    if not source_path.exists():
-        print(f"[ERROR] Source DB file not found at {source_path}")
-        return {'error': 'Source DB file not found'}, 500
-    # Nom du backup avec timestamp
-    timestamp = datetime.now().strftime("%Y_%m_%d")
-    backup_filename = f"metabase_backup_{timestamp}.mv.db"
-    destination_path = output_dir / backup_filename
+
+def stop_metabase():
+    print("[INFO] Stop Metabase docker container")
+    subprocess.run(["docker", "stop", "metabase"], check=True)
+    return "done"
+
+def start_metabase():
+    print("[INFO] Start Metabase docker container")
+    subprocess.run(["docker", "start", "metabase"], check=True)
+    return "done"
+
+def delete_metabase_db_in_container(container_name="metabase", db_path="/metabase.db"):
+    print(f"[INFO] Delete directory {db_path} inside container {container_name}...")
     try:
-        shutil.copy(source_path, destination_path)
-        print(f"[INFO] Metabase DB copied to {destination_path}")
-        return {'message': 'Backup done', 'file': str(destination_path)}
-    except Exception as e:
-        print(f"[ERROR] Failed to copy DB: {e}")
-        return {'error': str(e)}, 500
+        subprocess.run(["docker", "exec", container_name, "rm", "-rf", db_path], check=True)
+        print("[INFO] Deletion successful inside the container.")
+    except subprocess.CalledProcessError as e:
+        print(f"[ERROR] Failed to delete inside the container: {e}")
+    return "done"
+
+
+def copy_metabase_db_from_container():
+    os.makedirs("./outputs", exist_ok=True)
+    date_str = datetime.now().strftime("%Y_%m_%d")
+    file_name = "metabase.db_" + date_str
+    destination_path = Path("./outputs") / file_name
+    print(f"[INFO] Copying '/metabase.db' from container metabase to {destination_path} on host...")
+    try:
+        # Remove destination folder if it already exists (optional)
+        if destination_path.exists():
+            print(f"[INFO] Removing existing directory {destination_path}...")
+            subprocess.run(["rm", "-rf", str(destination_path)], check=True)
+        # Copy folder from container to host
+        subprocess.run(["docker", "cp", "metabase:/metabase.db", str(destination_path)], check=True)
+        print("[INFO] Copy completed successfully.")
+    except subprocess.CalledProcessError as e:
+        print(f"[ERROR] Failed to copy directory from container: {e}")
+    return "done"
--- Projet_master_RNCP/project_master_METABASE/App/views.py
+++ Projet_master_RNCP/project_master_METABASE/App/views.py
@@ -29,7 +29,27 @@ def api_save_metabase_db_to_S3():
         print(f"Job is already running. Skipping execution at {datetime.now()}")
         return {'message': 'Job already running'}, 200
     try:
-        utils.extract_metabase_db()
+        print("do stuffs")
         return "done"
     finally:
         lockfile.release_lock(fd, lockfile_name)
+
+@app.route('/stop_metabase', methods=["GET"])
+def api_stop_metabase():
+    utils.stop_metabase()
+    return "done"
+
+@app.route('/launch_metabase', methods=["GET"])
+def api_launch_metabase():
+    utils.start_metabase()
+    return "done"
+
+@app.route('/delete_metabase_db_in_container', methods=["GET"])
+def api_metabase_db_in_container():
+    utils.delete_metabase_db_in_container()
+    return "done"
+
+@app.route('/copy_metabase_db_from_container', methods=["GET"])
+def api_copy_metabase_db_from_container():
+    utils.copy_metabase_db_from_container()
+    return "done"
